c9fab3c0f138a298ba36f5a00aa43a19f0387cc0
reiy3s <reyes.am@obmontessori.edu.ph>
2026-09-03 20:03:15 +0800

    fix

--- main.py
+++ main.py
@@ -18,8 +18,6 @@ req_keys = {
 fru1ts = {"mango", "melon", "longan", "banana", "grapes"} #set
 d4y5 = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday") #tuple
 
-
-
 display(type(n4m3), target="div1")
 display(type(_4ge), target="div1")
 display(type(h31g7), target="div1")
@@ -28,3 +26,10 @@ display(type(n3w_s7ud3nt), target="div1")
 display(type(req_keys), target="div1")
 display(type(fru1ts), target="div1")
 display(type(d4y5), target="div1")
+
+def adding_numbers(e):
+    document.getElementById("numbers").innerHTML="" # clears previous output
+    num1 = float(document.getElementById('input1').value) # get 1st input
+    num2 = float(document.getElementById('input2').value) # get 2nd input
+    result = num1 + num2 # use operator to compute
+    display(result, target = "numbers") #display output in div
