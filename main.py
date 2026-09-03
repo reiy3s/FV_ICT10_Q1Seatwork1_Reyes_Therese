2a05d588d609d7eacb35d3748307afacda1b32ff
reiy3s <reyes.am@obmontessori.edu.ph>
2026-09-03 19:45:22 +0800

    fix

---
+++ main.py
@@ -0,0 +1,30 @@
+from pyscript import display
+
+# display("Detemine the types below!", target="div1") #literal object
+
+#variable objects
+
+n4m3 = "Therese" #string
+_4ge = 15 #int
+h31g7 = 157.48 #float
+_countries = ['Japan', 'Singapore', 'South Korea'] #list
+n3w_s7ud3nt = False #boolean
+req_keys = {
+    "color": "Lavender",
+    "car_brand": "Miata",
+    "shoe_size": "36",
+    "best_friend": "Zoey" #DONT TELL HER I LOVE ZOEY <3
+} #dictionary
+fru1ts = {mango, melon, longan, banana, grapes} #set
+d4y5 = ("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday") #tuple
+
+
+
+display(type(n4m3), target="div1")
+#display(type(b), target="div1")
+#display(type(c), target="div1")
+#display(type(d), target="div1")
+#display(type(e), target="div1")
+#display(type(f), target="div1")
+#display(type(g), target="div1")
+#display(type(h), target="div1")
